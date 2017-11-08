cbc09dfc1c49ed08c79b5ae96a2d11460ddbdfd4
rajeswari1203 <22810302+rajeswari1203@users.noreply.github.com>
2017-11-08 17:36:16 +0530

    test

--- aws.tf
+++ aws.tf
@@ -1,9 +1,10 @@
 provider "aws" {
-  access_key = "AKIAIBVIOMJRRAKXF7VA"
-  secret_key = "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"
+  access_key = "AKIAIIUVJUF4ARVBQPIA"
+  secret_key = "5GnwA1qBxn8vEOhjrLlp7isihmNZzFjK2xUtJNzS"
   region     = "ap-south-1"
   }
 resource "aws_instance" "example" {
   ami           = "ami-4fc58420"
   instance_type = "t2.micro"
 }
+
